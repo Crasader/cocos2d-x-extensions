3f95761bddd2b0c16b7f6225c398c07e9e6af170
yalab <rudeboyjet@gmail.com>
2015-06-09 21:57:20 +0900

    Implmenet supportfunctions::to_int.

--- SupportFunctions.h
+++ SupportFunctions.h
@@ -3,10 +3,19 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 namespace supportfunctions{
     int bit(const int num);
     std::string to_string(int i);
+
+    inline int to_int(const std::string& s) {
+        int v;
+        std::istringstream sin(s);
+        sin >> v;
+        return v;
+    }
+
     std::vector<std::string> splitLines(const std::string& str, const int rows);
     std::string strftime(const int timestamp, const char* format);
 }
