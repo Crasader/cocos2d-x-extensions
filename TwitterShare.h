d42ada2462d0ab37bd7624b4fbdf74a81b196cfe
yalab <rudeboyjet@gmail.com>
2015-11-03 22:50:29 +0900

    Write TwitterShare

---
+++ TwitterShare.h
@@ -0,0 +1,24 @@
+//
+//  TwitterShare.h
+//  まじょのおしごと
+//
+//  Created by Atsushi Yoshida on 2015/11/03.
+//
+//
+
+#ifndef __okeya__TwitterShare__
+#define __okeya__TwitterShare__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+using namespace cocos2d;
+using namespace cocos2d::ui;
+
+class TwitterShare
+{
+public:
+    void appendButton(Scene* scene);
+};
+
+#endif /* defined(__okeya__TwitterShare__) */
